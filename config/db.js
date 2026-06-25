bf0d840b5d5a54f6ddb8e4f9fc3ee44d122de54d
mitalijoshi30-tech <mitzi.joshi30@gmail.com>
2026-06-25 19:35:46 +0530

    new db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ let { MongoClient } = require("mongodb");
 let client = new MongoClient(process.env.MONGO_URL);
 client.connect();
 
-let db = client.db("MERN");
+let db = client.db("project1");
 
 module.exports = {
   messageCollec: db.collection("messages"),
